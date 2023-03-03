97c20c1216a168c687e406860886ee3d654854b6
khabaznia <khabaznia@live.com>
2023-03-03 21:48:25 +0100

    final patient master detail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Test3Component } from './test3/test3.component';
 import { FormsModule } from '@angular/forms';
-import { Test4Component } from './test4/test4.component';
 import { PatientDetailComponent } from './patient-detail/patient-detail.component';
 import { PatientListComponent } from './patient-list/patient-list.component';
-import { Test4DetailComponent } from './test4-detail/test4-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Test3Component,
-    Test4Component,
     PatientDetailComponent,
-    PatientListComponent,
-    Test4DetailComponent
+    PatientListComponent
   ],
   imports: [
     BrowserModule,
